c30b5798c66aa55136d3d708f2a7d369b536c4e4
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2020-12-16 13:08:06 +0100

    White-Box

--- src/main/java/org/bschlangaul/aufgaben/sosy/white_box/WhiteBox.java
+++ src/main/java/org/bschlangaul/aufgaben/sosy/white_box/WhiteBox.java
@@ -4,24 +4,27 @@ public class WhiteBox {
   public int ggT(int a, int b) {
     int result = 1;
     for (int i = 1; i <= Math.min(a, b); i++) {
-      if((a % i == 0) & (b % i == 0)) {
+      if ((a % i == 0) & (b % i == 0)) {
         result = i;
       }
     }
-    System.out.println(result);
     return result;
   }
 
-  public static void main(String[] args) {
+  public static void debug(int a, int b) {
     WhiteBox w = new WhiteBox();
-    w.ggT(1,2); // 1
-    w.ggT(10,2); // 2
-    w.ggT(4,7); // 1
-    w.ggT(6,2); // 1
-    w.ggT(5,2); // 1
-    w.ggT(101,27); // 1
-    w.ggT(12,27); // 3
-    w.ggT(10,20); // 10
-    w.ggT(-1,-2); // 1
+    System.out.println(w.ggT(a, b));
+  }
+
+  public static void main(String[] args) {
+    debug(1, 2); // 1
+    debug(10, 2); // 2
+    debug(4, 7); // 1
+    debug(6, 2); // 1
+    debug(5, 2); // 1
+    debug(101, 27); // 1
+    debug(12, 27); // 3
+    debug(10, 20); // 10
+    debug(-1, -2); // 1
   }
 }
