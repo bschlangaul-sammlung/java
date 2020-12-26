ec3a4ec0f2da8a29e89f6d0217933db02899ace6
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2020-12-26 19:36:35 +0100

    Standardmäßig die Hilfe anzeigen

--- src/main/java/org/bschlangaul/cli/KommandoZeile.java
+++ src/main/java/org/bschlangaul/cli/KommandoZeile.java
@@ -2,19 +2,22 @@ package org.bschlangaul.cli;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-
+import picocli.CommandLine.Spec;
+import picocli.CommandLine.Model.CommandSpec;
 import java.util.concurrent.Callable;
 
 import static picocli.CommandLine.Model.UsageMessageSpec.SECTION_KEY_COMMAND_LIST;
 
-
 @Command(name = "didaktik", mixinStandardHelpOptions = true, version = "didaktik 0.1.0", description = "Kommandozeilen-Interface für die Java-Didaktik-Beispiele.", subcommands = {
     Baum.class, Graph.class })
 class KommandoZeile implements Callable<Integer> {
+  @Spec
+  CommandSpec spec;
 
   @Override
   public Integer call() {
     System.out.println("Benutze eine Unterkommando!");
+    new CommandLine(spec).usage(System.out);
     return 0;
   }
 
