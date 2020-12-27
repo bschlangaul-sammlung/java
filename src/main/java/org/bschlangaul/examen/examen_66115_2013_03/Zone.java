2ed01af90cb0bc17d9ec0858ec43b1c5f3f19fbb
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2020-12-27 19:32:22 +0100

    Examensaufgabe

--- src/main/java/org/bschlangaul/examen/examen_66115_2013_03/Zone.java
+++ src/main/java/org/bschlangaul/examen/examen_66115_2013_03/Zone.java
@@ -3,13 +3,54 @@ package org.bschlangaul.examen.examen_66115_2013_03;
 import java.util.ArrayList;
 
 class Zone {
+
   private ArrayList<ArrayList<String>> urlList = new ArrayList<ArrayList<String>>();
+
   private ArrayList<ArrayList<String>> ipList = new ArrayList<ArrayList<String>>();
 
-  public int hash(String url) {
-    return 1;
-    /* calculates hash-value h, >=0 */
+  /**
+   * Der Konstruktor initialisert die Streutabellen mit 46 Buckets, damit wir den
+   * Code testen können.
+   */
+  public Zone() {
+    for (int i = 0; i <= 45; i++) {
+      urlList.add(new ArrayList<String>());
+      ipList.add(new ArrayList<String>());
+    }
+  }
 
+  /**
+   * Diese Methode wird zum Testen der Methode getIndex gebraucht.
+   */
+  public ArrayList<String> getUrlCollisionList(int h) {
+    return urlList.get(h);
+  }
+
+  /**
+   * Nicht wirklich eine Hashfunktion. Gibt die Werte wie im Schaubild zurück.
+   * Diese Methode muss nicht implementiert werden. Sie ist nur dazu da, um die
+   * Code testen zu können.
+   *
+   * @param url Eine URL.
+   *
+   * @return Ein Hashwert, der größer gleich 0 ist.
+   */
+  public int hash(String url) {
+    /* calculates hash-value h, >=0 */
+    switch (url) {
+      case "google.de":
+        return 0;
+
+      case "bayern.de":
+        return 1;
+
+      case "facebook.com":
+      case "gmx.net":
+        return 45;
+
+      default:
+        return 42;
+    }
   }
 
   /**
@@ -17,10 +58,20 @@ class Zone {
    * vorhanden ist. Falls h größer ist als die derzeitige Größe der Streutabelle,
    * existiert der Eintrag nicht.
    *
-   * @param h
-   * @return
+   * @param h Der Index-Wert, der durch die Hashfunktion erzeugt wird.
+   *
+   * @return Wahr, wenn in beiden Streutabellen an einer bestimmen Index-Position
+   *         mindestes ein Wert hinterlegt ist, sonst falsch.
    */
   boolean exists(int h) {
+    if (urlList.size() - 1 < h || ipList.size() - 1 < h)
+      return false;
+
+    ArrayList<String> urlCollisionList = urlList.get(h);
+    ArrayList<String> ipCollisionList = ipList.get(h);
+    if (urlCollisionList.size() == 0 || ipCollisionList.size() == 0)
+      return false;
+
     return true;
   }
 
@@ -29,19 +80,46 @@ class Zone {
    * Kollisionsliste nicht vorhanden, soll -1 zurückgeliefert werden.
    */
   int getIndex(String url, ArrayList<String> urlList) {
-    return 0;
-
+    for (int i = 0; i < urlList.size(); i++) {
+      if (urlList.get(i).equals(url))
+        return i;
+    }
+    return -1;
   }
 
   /**
    * Gib in der Streutabelle die IP-Adresse zurück. Wird eine nicht vorhandene
    * Adresse abgerufen, wird eine Fehlermeldung zurückgegeben.
    *
-   * @param url
-   * @return
+   * @param url Die gesuchte URL.
+   *
+   * @return Die entsprechende IP-Adresse.
    */
   String lookup(String url) {
-    return "lol";
+    int h = hash(url);
+    int collisionIndex = getIndex(url, urlList.get(h));
+    if (collisionIndex == -1)
+      return "Die URL kannte nicht in der Tabelle gefunden werden";
+    return ipList.get(h).get(collisionIndex);
   }
 
+  /**
+   * Nicht verlangt. Zum Einfügen von Testwerten gedacht.
+   *
+   * @param url Eine URL.
+   * @param ip  Eine IP-Adresse.
+   */
+  public void addUrl(String url, String ip) {
+    int h = hash(url);
+    urlList.get(h).add(url);
+    ipList.get(h).add(ip);
+  }
+
+  public static void main(String[] args) {
+    Zone zone = new Zone();
+    zone.addUrl("google.de", "173.194.69.9");
+    zone.addUrl("bayern.de", "195.200.71.1");
+    zone.addUrl("facebook.com", "69.171.229.1");
+    zone.addUrl("gmx.net", "213.165.64.7");
+  }
 }
