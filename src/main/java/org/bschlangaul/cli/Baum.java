50a61247c1a8e51cdb0ef96e8c93efc65ba90658
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2020-12-28 18:47:08 +0100

    Mit Baumen rum spielen

--- src/main/java/org/bschlangaul/cli/Baum.java
+++ src/main/java/org/bschlangaul/cli/Baum.java
@@ -3,9 +3,14 @@ package org.bschlangaul.cli;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import tech.vanyo.tree_printer.TreePrinter;
+import tech.vanyo.tree_printer.TreeNode;
 
 import java.util.List;
 import java.util.concurrent.Callable;
+
+import org.bschlangaul.baum.BinaerBaum;
+import org.bschlangaul.baum.Knoten;
 
 @Command(name = "baum", aliases = {
     "b" }, mixinStandardHelpOptions = true, description = "Führe baumspezifische Aufgaben aus.")
@@ -19,10 +24,21 @@ class Baum implements Callable<Integer> {
 
   @Override
   public Integer call() {
-    for (int i = 0; i < werte.size(); i++) {
-      System.out.println(werte.get(i));
-    }
-    System.out.println(isAvl);
+
+    // BinaerBaum baum = new BinaerBaum();
+    // baum.fügeEin(15, 7, 2, 8, 19, 29);
+    // Knoten knoten = baum.gibKopf();
+
+    TreeNode root = new TreeNode(3);
+    root.setLeft(new TreeNode(1));
+    root.setRight(new TreeNode(5));
+
+    TreePrinter<TreeNode> printer = new TreePrinter<TreeNode>(n -> ("" + n.getValue()), n -> n.getLeft(), n -> n.getRight());
+    printer.printTree(root);
+    // for (int i = 0; i < werte.size(); i++) {
+    //   System.out.println(werte.get(i));
+    // }
+    // System.out.println(isAvl);
     return 0;
   }
 }
