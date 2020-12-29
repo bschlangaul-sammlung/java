cd6ec374f689f0d4b3978cb32df6aa0992c5a980
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2020-12-29 17:53:52 +0100

    ANTLR verbessern

--- src/main/antlr4/org/bschlangaul/antlr/Graph.g4
+++ src/main/antlr4/org/bschlangaul/antlr/Graph.g4
@@ -2,7 +2,7 @@ grammar Graph;
 
 graph: zeile+ (knoten | kante)? EOF;
 
-zeile: (knoten | kante) ZEILEN_ENDE;
+zeile: (knoten | kante) TRENNER;
 
 kante:
 	von ' '* (gerichtet | ungerichtet) ' '* nach (
@@ -14,18 +14,21 @@ von: KNOTENNAME;
 nach: KNOTENNAME;
 gewicht: GLEITZAHL;
 
-knoten: name ' '* ':' ' '* x ' '+ y;
+knoten: name ' '* (':' ' '* x ' '+ y)?;
 name: KNOTENNAME;
 x: GLEITZAHL;
 y: GLEITZAHL;
 
-fragment BUCHSTABE: [A-Za-z];
+fragment BUCHSTABE: [A-Za-z\p{Block=Latin_1_Supplement}];
 fragment ZAHL: [0-9];
+
+KOMMENTAR: [ \t]* '%' ~[\r\n;]* TRENNER -> skip;
+
+KNOTENNAME: BUCHSTABE (BUCHSTABE | ZAHL | '_')*;
 
 GLEITZAHL: DEZIMALZAHL | GANZZAHL;
 DEZIMALZAHL: '-'? GANZZAHL '.' GANZZAHL;
 GANZZAHL: '-'? ZAHL+;
 
-KNOTENNAME: BUCHSTABE+;
-ZEILEN_ENDE: ('\r'? '\n' | '\r' | ';')+ ' '*;
+TRENNER: ('\r'? '\n' | '\r' | ';')+ ' '*;
 LEERZEICHEN: [ \t]+ -> skip;
--- src/main/antlr4/org/bschlangaul/antlr/debug.sh
+++ src/main/antlr4/org/bschlangaul/antlr/debug.sh
@@ -1,9 +1,26 @@
-#! /bin/sh
+#! /bin/bash
 
-rm -f *.java
 
-START_RULE_NAME="einstiegs_punkt"
-GRAMMAR_NAME="RelationenSchema"
+if [ -z "$2" ]; then
+  echo "Usage: $0 GRAMMAR_NAME START_RULE_NAME"
+  exit 1
+fi
+
+
+_clean_up() {
+  echo "clean up ..."
+  rm -f *.java
+  rm -f *.tokens
+  rm -f *.interp
+  rm -f *.class
+}
+
+_clean_up
+
+GRAMMAR_NAME="$1"
+START_RULE_NAME="$2"
+DEBUG_FILE_SUFFIX="$3"
+
 JAR="/usr/local/lib/antlr-4.9-complete.jar"
 
 # antlr4
@@ -12,6 +29,16 @@ java -Xmx500M -cp "$JAR:$CLASSPATH" org.antlr.v4.Tool "$GRAMMAR_NAME".g4
 # javac
 javac -cp "$JAR" "$GRAMMAR_NAME"*.java
 
+DEBUG_FILE="$GRAMMAR_NAME".debug.txt
+
+if [ -n "$DEBUG_FILE_SUFFIX" ]; then
+  DEBUG_FILE="$GRAMMAR_NAME".debug."$DEBUG_FILE_SUFFIX".txt
+else
+  DEBUG_FILE="$GRAMMAR_NAME".debug.txt
+fi
+
 # grun
 java -Xmx500M -cp "$JAR:$CLASSPATH" org.antlr.v4.gui.TestRig \
-  "$GRAMMAR_NAME" "$START_RULE_NAME" "$GRAMMAR_NAME".debug.txt -gui
+  "$GRAMMAR_NAME" "$START_RULE_NAME" "$DEBUG_FILE" -gui
+
+_clean_up
