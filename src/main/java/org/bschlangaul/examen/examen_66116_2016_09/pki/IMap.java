ff26e6433ecf5ad4244ba1f93b576474ca1e411b
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-01-07 19:22:04 +0100

    Examensaufgabe

---
+++ src/main/java/org/bschlangaul/examen/examen_66116_2016_09/pki/IMap.java
@@ -0,0 +1,7 @@
+package org.bschlangaul.examen.examen_66116_2016_09.pki;
+
+interface IMap {
+  Object get(String key);
+
+  void put(String key, Object value);
+}
---
+++ src/main/java/org/bschlangaul/examen/examen_66116_2016_09/pki/MapEntry.java
@@ -0,0 +1,14 @@
+package org.bschlangaul.examen.examen_66116_2016_09.pki;
+
+public class MapEntry {
+
+  String key;
+  Object value;
+
+  MapEntry(String key, Object value) {
+    // Nicht verlangt in der Aufgabenstellung.
+    this.key = key;
+    this.value = value;
+  }
+
+}
