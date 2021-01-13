18d591e2fca10684601543b0d248b23654eacfc8
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-01-13 12:34:52 +0100

    Schreibtischlauf

--- src/main/java/org/bschlangaul/aufgaben/tech_info/schreibtischlauf/ThreadSafety.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/schreibtischlauf/ThreadSafety.java
@@ -13,10 +13,51 @@ import java.util.concurrent.atomic.*;
  * Datei Schreibtischlauf.pdf ab.
  *
  * <ol>
- * <li>Welche Ausgaben sind für v möglich? Begründe deine Antwort.
- * <li>Welche Ausgaben sind für a möglich? Begründe deine Antwort.
+ * <li>Welche Ausgaben sind für v möglich? Begründe deine Antwort:
+ *
+ * <p>
+ * Meistens 2, in seltenen Fällen 1. v ist zwar mit volatile versehen worden, d.
+ * h. Änderungen werden auch für andere Threads sichtbar, jedoch kann es
+ * vorkommen, dass die beiden Threads gleichzeitig von 0 aus hochzählen und
+ * zweimal 1 setzen.
+ *
+ * <li>Welche Ausgaben sind für a möglich? Begründe deine Antwort:
+ *
+ * <p>
+ * Für das Attribut {@code a} ist die Ausgabe 1 oder 2 möglich. {@code a} ist
+ * eine Instance von {@link AtomicInteger}, d. h. die Klassen {@link T1} und
+ * {@link T2} könnten nicht gleichzeitig {@code a} hochzählen. Eine Klasse
+ * erhält den Vorrang. Hat {@code a} den Wert 1 erreicht, verhindert die
+ * Bedingung {@code a.get() < 1}, dass weiter hochgezählt wird. Es kann jedoch
+ * sein, dass die beiden Threads gleichzeitig das Innere der if-Bedingung (jetzt
+ * hätte ich beinahe Schleife geschrieben ...) erreichen, und dann nacheinander
+ * auf 2 hochzählen.
+ *
  * <li>Welche Ausgaben sind für x möglich? Begründe deine Antwort.
+ *
+ * <p>
+ * Für das Attribut {@code x} ist nur die Ausgabe 4 möglich. Die beiden Klassen
+ * {@link T1} und {@link T2} enthalten eine Wiederholung mit fester Anzahl, die
+ * x zwei mal hochzählen. Das Hochzählen von x ist in beiden Klassen in einem
+ * mit synchronized geschützen Codebereich eingebunden, sodass der
+ * Hochzählprozess exclusive von einem Thread abgearbeitet werden kann, der
+ * andere Thread aufgrund des Locks warten muss. Es ist unerheblich, ob die
+ * Wiederholung mit fester Anzahl den synchronized-Codeblock nur umschließt oder
+ * in einen synchronized-Block eingebunden ist. Entscheidend ist, dass sich die
+ * Zeile {@code x++;} im synchronized-Codeblock befindet.
+ *
  * <li>Welche Ausgaben sind für y möglich? Begründe deine Antwort.
+ *
+ * <p>
+ * Für das Attribut {@code y} ist nur die Ausgabe 42 möglich. Die run-Methode
+ * der Klasse T1 setzt das Attribut z auf den Wert 40. Aufgrund der „Happens
+ * Before Guarantee“ von Java wirkt sich das volatile vor dem Attribut b auch
+ * auf das Attribut z aus. Das Attribut z wird erst nachdem b auf true gesetzt
+ * wird, atualisiert. Solange hängt die run-Methode des Klasse T2 in dem
+ * while-Loop fest. Aufgrund des volatiles-Schlüsselworts vor b erfährt T2 von
+ * der Wertänderung und verlässt die Schleife und wegen der Happens Befor
+ * Guarantee ist z auch volatile. In der Zeile y = x + 2; wird y schließlich auf
+ * 42 gesetzt
  * </ol>
  */
 public class ThreadSafety {
@@ -76,16 +117,21 @@ public class ThreadSafety {
     t1.join();
     t2.join();
 
-    System.out.println("v = " + v);
-    System.out.println("a = " + a.get());
-    System.out.println("x = " + x);
-    System.out.println("y = " + y);
+    // System.out.println("v = " + v);
+    // System.out.println("a = " + a.get());
+    // System.out.println("x = " + x);
+    // System.out.println("y = " + y);
+    if (v != 2)
+      System.out.println("v = " + v); // 1 oder 2
+    // if (a.get() != 1) System.out.println("a = " + a.get()); // 1 oder 2 (16x a )
+    // if (x != 4) System.out.println("x = " + x);
+    // if (y != 42) System.out.println("y = " + y); // immer 42
   }
 
   public static void main(String[] args) throws Exception {
     ThreadSafety ts = new ThreadSafety();
     ts.execute();
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 1000000; i++) {
       ts = new ThreadSafety();
       ts.execute();
     }
