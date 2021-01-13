1355ed3c14f9ea18cdd1050fee13acb573c6a132
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-01-13 21:51:46 +0100

    Javadoc

--- src/main/java/org/bschlangaul/aufgaben/tech_info/schreibtischlauf/ThreadSafety.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/schreibtischlauf/ThreadSafety.java
@@ -16,18 +16,18 @@ import java.util.concurrent.atomic.*;
  * <li>Welche Ausgaben sind für v möglich? Begründe deine Antwort:
  *
  * <p>
- * Meistens 2, in seltenen Fällen 1. v ist zwar mit volatile versehen worden, d.
- * h. Änderungen werden auch für andere Threads sichtbar, jedoch kann es
- * vorkommen, dass die beiden Threads gleichzeitig von 0 aus hochzählen und
- * zweimal 1 setzen.
+ * Meistens 2, in seltenen Fällen 1. {@link v} ist zwar mit {@code volatile}
+ * versehen worden, d. h. Änderungen werden auch für andere Threads sichtbar,
+ * jedoch kann es vorkommen, dass die beiden Threads gleichzeitig zu der Zeile
+ * {@code v++;} kommen und beide von 0 aus hochzählen und zweimal 1 setzen.
  *
  * <li>Welche Ausgaben sind für a möglich? Begründe deine Antwort:
  *
  * <p>
- * Für das Attribut {@code a} ist die Ausgabe 1 oder 2 möglich. {@code a} ist
- * eine Instance von {@link AtomicInteger}, d. h. die Klassen {@link T1} und
- * {@link T2} könnten nicht gleichzeitig {@code a} hochzählen. Eine Klasse
- * erhält den Vorrang. Hat {@code a} den Wert 1 erreicht, verhindert die
+ * Für das Attribut {@link a} ist die Ausgabe 1 oder 2 möglich. {@link a} ist
+ * eine Instanze von {@code AtomicInteger}, d. h. die Klassen {@link T1} und
+ * {@link T2} können nicht gleichzeitig {@link a} hochzählen. Eine Klasse
+ * erhält den Vorrang. Hat {@link a} den Wert 1 erreicht, verhindert die
  * Bedingung {@code a.get() < 1}, dass weiter hochgezählt wird. Es kann jedoch
  * sein, dass die beiden Threads gleichzeitig das Innere der if-Bedingung (jetzt
  * hätte ich beinahe Schleife geschrieben ...) erreichen, und dann nacheinander
@@ -36,11 +36,11 @@ import java.util.concurrent.atomic.*;
  * <li>Welche Ausgaben sind für x möglich? Begründe deine Antwort.
  *
  * <p>
- * Für das Attribut {@code x} ist nur die Ausgabe 4 möglich. Die beiden Klassen
+ * Für das Attribut {@link x} ist nur die Ausgabe 4 möglich. Die beiden Klassen
  * {@link T1} und {@link T2} enthalten eine Wiederholung mit fester Anzahl, die
- * x zwei mal hochzählen. Das Hochzählen von x ist in beiden Klassen in einem
- * mit synchronized geschützen Codebereich eingebunden, sodass der
- * Hochzählprozess exclusive von einem Thread abgearbeitet werden kann, der
+ * {@link x} zwei mal hochzählen. Das Hochzählen von {@link x} ist in beiden
+ * Klassen in einem mit synchronized geschützen Codebereich eingebunden, sodass
+ * der Hochzählprozess exklusiv von einem Thread abgearbeitet werden kann, der
  * andere Thread aufgrund des Locks warten muss. Es ist unerheblich, ob die
  * Wiederholung mit fester Anzahl den synchronized-Codeblock nur umschließt oder
  * in einen synchronized-Block eingebunden ist. Entscheidend ist, dass sich die
@@ -50,14 +50,16 @@ import java.util.concurrent.atomic.*;
  *
  * <p>
  * Für das Attribut {@code y} ist nur die Ausgabe 42 möglich. Die run-Methode
- * der Klasse T1 setzt das Attribut z auf den Wert 40. Aufgrund der „Happens
- * Before Guarantee“ von Java wirkt sich das volatile vor dem Attribut b auch
- * auf das Attribut z aus. Das Attribut z wird erst nachdem b auf true gesetzt
- * wird, atualisiert. Solange hängt die run-Methode des Klasse T2 in dem
- * while-Loop fest. Aufgrund des volatiles-Schlüsselworts vor b erfährt T2 von
- * der Wertänderung und verlässt die Schleife und wegen der Happens Befor
- * Guarantee ist z auch volatile. In der Zeile y = x + 2; wird y schließlich auf
- * 42 gesetzt
+ * der Klasse {@link T1} setzt das Attribut {@link z} auf den Wert 40. Aufgrund
+ * der „Happens Before Guarantee“
+ * (http://tutorials.jenkov.com/java-concurrency/volatile.html) von Java wirkt
+ * sich das {@code volatile} vor dem Attribut {@code b} auch auf das Attribut
+ * {@link z} aus. Das Attribut {@code z} wird erst nachdem {@code b} auf true
+ * gesetzt wird, aktualisiert - solange hängt die run-Methode der Klasse
+ * {@link T2} in dem while-Loop fest. Aufgrund des volatile-Schlüsselworts vor
+ * {@link b} erfährt {@link T2} von der Wertänderung und verlässt die Schleife
+ * und wegen der „Happens Before Guarantee“ ist {@link z} auch volatile. In der
+ * Zeile {@code y = x + 2;} wird {@link y} schließlich auf 42 gesetzt
  * </ol>
  */
 public class ThreadSafety {
