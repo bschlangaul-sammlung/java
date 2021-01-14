6aecb512b35da3e064cad88fe53e0bca65b8c7b2
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-01-14 22:27:24 +0100

    MonteCarlo

--- src/main/java/org/bschlangaul/aufgaben/tech_info/monte_carlo/MonteCarloImpl.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/monte_carlo/MonteCarloImpl.java
@@ -6,6 +6,15 @@ public class MonteCarloImpl implements MonteCarlo {
 
   Random zufall = new Random();
 
+  /**
+   * Kleine Hilfsfunktions, um einen Bereich spezifizeren zu könnten, aus den man
+   * Zufallszahlen will.
+   *
+   * @param min Die Zufallszahl soll größer gleich als diese Zahl sein.
+   * @param max Die Zufallszahl soll kleiner als diese Zahl sein.
+   *
+   * @return Eine zufällige Zahl.
+   */
   private double gibZufallsZahl(double min, double max) {
     return min + (max - min) * zufall.nextDouble();
   }
@@ -13,31 +22,38 @@ public class MonteCarloImpl implements MonteCarlo {
   /**
    * {@inheritDoc}
    */
-  public double computeIntegral(Function f, double minX, double maxX, double minY, double maxY, int threads,
+  public double computeIntegral(Function f, double xMin, double xMax, double yMin, double yMax, int threads,
       long iterations) {
+    // Berechnung des Bereichs für die Zufallszahlen auf der Y-Achse.
+    // Das Integral ist ja die Fläche von der X-Achse (y=0) zur Linie des Graphen.
+    // Es muss mindestens einmal null vorkommen, entweder beim minimalen Y oder beim
+    // maximalen Y.
+    yMin = yMin > 0 ? 0 : yMin;
+    yMax = yMax < 0 ? 0 : yMax;
 
     long treffer = 0;
     for (int i = 0; i <= iterations; i++) {
-      double x = gibZufallsZahl(minX, maxX);
-      double y = gibZufallsZahl(minY, maxY);
+      double x = gibZufallsZahl(xMin, xMax);
+      double y = gibZufallsZahl(yMin, yMax);
       double berechntesY = f.compute(x);
-      //System.out.println(berechntesY);
-      if (berechntesY > y && y > 0) {
+      if (berechntesY >= y && y >= 0) {
         treffer++;
       } else if (berechntesY < y && y < 0) {
         treffer--;
       }
     }
-          System.out.println(treffer);
-
-    return (double) treffer / iterations;
+    double flächeZufallsViereck = (xMax - xMin) * (yMax - yMin);
+    // Diesen cast braucht man, da int bzw. long durch int/long wieder int/long
+    // ergibt, hier also 0.
+    double anteilTreffer = (double) treffer / iterations;
+    return flächeZufallsViereck * anteilTreffer;
   }
 
   public static void main(String[] args) {
     MonteCarloImpl m = new MonteCarloImpl();
     Kreis k = new Kreis();
     Polynomial p1 = new Polynomial(new double[] { 0, 1 });
-    //double ergebnis = m.computeIntegral(k, -1, 1, 0, k.compute(1), 8, 1000000);
+    // double ergebnis = m.computeIntegral(k, -1, 1, 0, k.compute(1), 8, 1000000);
     double ergebnis = m.computeIntegral(p1, 0, 1, 0, p1.compute(1), 8, 1000000);
     System.out.printf("Ergebnis: %f", ergebnis);
 
