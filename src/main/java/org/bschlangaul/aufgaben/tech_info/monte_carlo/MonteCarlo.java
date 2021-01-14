687f70065559ac23f90fc7579949e00e86372468
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-01-14 19:38:32 +0100

    Monte Carlo

--- src/main/java/org/bschlangaul/aufgaben/tech_info/monte_carlo/MonteCarlo.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/monte_carlo/MonteCarlo.java
@@ -3,16 +3,19 @@ package org.bschlangaul.aufgaben.tech_info.monte_carlo;
 public interface MonteCarlo {
 
   /**
-   * Computes the integral over f from a to b.
+   * Berechne das Integral der Funktion f von a nach b.
    *
-   * @param f          the function to integrate
+   * @param f          Die Integral-Funktion.
    * @param a          interval a
    * @param b          interval b
-   * @param minY       lowest function value in the interval
-   * @param maxY       highest function value in the interval
-   * @param threads    number of additional threads to use
-   * @param iterations the total number of random points used for the calculation
-   * @return the integral size
+   * @param minY       Der kleines Y-Wert der Funktion im Ausschnitt.
+   * @param maxY       Der größte Y-Wert der Funktion im Ausschnitt.
+   * @param threads    Anzahl der zusätzlichen Threads, die zur Berechnung
+   *                   verwendent werden soll.
+   * @param iterations Die Gesamtzahl an zufälligen Punkten, die zur Berechnung
+   *                   herangezogen werden sollen.
+   *
+   * @return Die Größe des Integrals.
    */
   public double computeIntegral(Function f, double a, double b, double minY, double maxY, int threads, long iterations);
 
--- src/main/java/org/bschlangaul/aufgaben/tech_info/monte_carlo/MonteCarloImpl.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/monte_carlo/MonteCarloImpl.java
@@ -1,32 +1,45 @@
 package org.bschlangaul.aufgaben.tech_info.monte_carlo;
 
+import java.util.Random;
+
 public class MonteCarloImpl implements MonteCarlo {
+
+  Random zufall = new Random();
+
+  private double gibZufallsZahl(double min, double max) {
+    return min + (max - min) * zufall.nextDouble();
+  }
+
   /**
    * {@inheritDoc}
    */
-  public double computeIntegral(Function f, double a, double b, double minY, double maxY, int threads,
+  public double computeIntegral(Function f, double minX, double maxX, double minY, double maxY, int threads,
       long iterations) {
-    return 1d;
+
+    long treffer = 0;
+    for (int i = 0; i <= iterations; i++) {
+      double x = gibZufallsZahl(minX, maxX);
+      double y = gibZufallsZahl(minY, maxY);
+      double berechntesY = f.compute(x);
+      //System.out.println(berechntesY);
+      if (berechntesY > y && y > 0) {
+        treffer++;
+      } else if (berechntesY < y && y < 0) {
+        treffer--;
+      }
+    }
+          System.out.println(treffer);
+
+    return (double) treffer / iterations;
   }
 
   public static void main(String[] args) {
-
-    // Gesamtzahl der Tropfen
-    int g = 10000000;
-    // Tropfen im Viertelkreis
-    int v = 0;
-    // Koordinaten des Punktes P
-    double x, y;
-    System.out.println(" Monte Carlo Methode");
-    System.out.println(" Naeherung fuer Pi:");
-    for (int i = 1; i <= g; i++) {
-      x = Math.random();
-      y = Math.random();
-      if (Math.hypot(x, y) <= 1)
-        v = v + 1;
-    }
-    double pi = 4 * (double) v / g;
-    System.out.printf(" %d Tropfen, davon %d Tropfen im Viertelkreis, Pi etwa %g%n", g, v, pi);
+    MonteCarloImpl m = new MonteCarloImpl();
+    Kreis k = new Kreis();
+    Polynomial p1 = new Polynomial(new double[] { 0, 1 });
+    //double ergebnis = m.computeIntegral(k, -1, 1, 0, k.compute(1), 8, 1000000);
+    double ergebnis = m.computeIntegral(p1, 0, 1, 0, p1.compute(1), 8, 1000000);
+    System.out.printf("Ergebnis: %f", ergebnis);
 
   }
 }
