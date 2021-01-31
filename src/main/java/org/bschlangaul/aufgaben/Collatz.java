a090239e67105090aec653b99be9ee8bfd890a4a
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-01-31 12:20:23 +0100

    Collatz

--- src/main/java/org/bschlangaul/aufgaben/Collatz.java
+++ src/main/java/org/bschlangaul/aufgaben/Collatz.java
@@ -3,13 +3,16 @@ package org.bschlangaul.aufgaben;
 public class Collatz {
   public static void main(String[] args) {
     int n = 7;
+    int anzahl = 0;
     while (n != 1) {
       if (n % 2 == 0) {
         n = n / 2;
       } else {
         n = 3 * n + 1;
       }
+      anzahl++;
       System.out.println(n);
     }
+    System.out.println("Anzahl an Durchläufen " + anzahl);
   }
 }
