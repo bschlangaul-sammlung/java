f4354c8b1f7d1336965d915b271960ec1b2dceef
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-02-04 18:06:42 +0100

    Primzahl

---
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/Primzahl.java
@@ -0,0 +1,49 @@
+package org.bschlangaul.aufgaben.tech_info.assembler;
+
+public class Primzahl {
+
+  public static boolean istPrimzahl(int k) {
+    if (k == 1)
+      return true;
+    int a = 0;
+    int t = 1;
+
+    do {
+      t++;
+      if (k % t == 0) {
+        a = 1;
+      }
+    } while (a == 0);
+
+    if (t == k) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void berichtePrimzahl(int k) {
+    String keine = "";
+    if (!istPrimzahl(k)) {
+      keine = "k";
+    }
+    System.out.println(String.format("Die Zahl %d ist %seine Primzahl", k, keine));
+  }
+
+
+  public static void main(String[] args) {
+    berichtePrimzahl(1);
+    berichtePrimzahl(2);
+    berichtePrimzahl(3);
+    berichtePrimzahl(4);
+    berichtePrimzahl(5);
+    berichtePrimzahl(6);
+    berichtePrimzahl(7);
+    berichtePrimzahl(8);
+    berichtePrimzahl(9);
+    berichtePrimzahl(10);
+    berichtePrimzahl(11);
+    berichtePrimzahl(12);
+    berichtePrimzahl(13);
+  }
+}
