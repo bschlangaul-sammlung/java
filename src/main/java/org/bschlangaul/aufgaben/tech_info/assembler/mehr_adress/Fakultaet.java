94c0f84d5128b53abafaa30932f0a4b5b3e7037c
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-02-09 21:35:42 +0100

    Fakultät

--- src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/mehr_adress/Fakultaet.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/mehr_adress/Fakultaet.java
@@ -2,16 +2,16 @@ package org.bschlangaul.aufgaben.tech_info.assembler.mehr_adress;
 
 public class Fakultaet {
 
-  private static int berechneFak(int n) {
+  private static int fak(int n) {
     if (n == 1) {
       return 1;
     } else {
-      return berechneFak(n - 1) * n;
+      return n * fak(n - 1);
     }
   }
 
   public static void main(String[] args) {
     int n = 7;
-    System.out.println(berechneFak(n));
+    System.out.println(fak(n));
   }
 }
