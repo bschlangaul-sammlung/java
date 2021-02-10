caa0ce96d668ba2aaf15d71ba1771eff2987d84a
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-02-10 18:10:57 +0100

    Java

--- src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/mehr_adress/SummeIterativ.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/mehr_adress/SummeIterativ.java
@@ -14,5 +14,17 @@ public class SummeIterativ {
   public static void main(String[] args) {
     int n = 7;
     System.out.println(summe(n)); // 28
+
+    System.out.println(summe(0)); // 0
+    System.out.println(summe(1)); // 1
+    System.out.println(summe(2)); // 3
+    System.out.println(summe(3)); // 6
+    System.out.println(summe(4)); // 10
+    System.out.println(summe(5)); // 15
+    System.out.println(summe(6)); // 21
+    System.out.println(summe(7)); // 28
+    System.out.println(summe(8)); // 36
+    System.out.println(summe(9)); // 45
+    System.out.println(summe(10)); // 55
   }
 }
