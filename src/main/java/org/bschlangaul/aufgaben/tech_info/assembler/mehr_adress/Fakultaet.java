953cfac4efad9ee02b72b26dacb0d2ad69f3eb0f
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-02-12 10:09:33 +0100

    Tests

--- src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/mehr_adress/Fakultaet.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/mehr_adress/Fakultaet.java
@@ -2,8 +2,8 @@ package org.bschlangaul.aufgaben.tech_info.assembler.mehr_adress;
 
 public class Fakultaet {
 
-  private static int fak(int n) {
-    if (n == 1) {
+  public static int fak(int n) {
+    if (n <= 1) {
       return 1;
     } else {
       return n * fak(n - 1);
@@ -13,5 +13,17 @@ public class Fakultaet {
   public static void main(String[] args) {
     int n = 7;
     System.out.println(fak(n));
+
+    System.out.println(fak(0)); // 1
+    System.out.println(fak(1)); // 1
+    System.out.println(fak(2)); // 2
+    System.out.println(fak(3)); // 6
+    System.out.println(fak(4)); // 24
+    System.out.println(fak(5)); // 120
+    System.out.println(fak(6)); // 720
+    System.out.println(fak(7)); // 5040
+    System.out.println(fak(8)); // 40320
+    System.out.println(fak(9)); // 362880
+    System.out.println(fak(10)); // 3628800
   }
 }
--- src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/mehr_adress/FibonacciRekursiv.java
+++ src/main/java/org/bschlangaul/aufgaben/tech_info/assembler/mehr_adress/FibonacciRekursiv.java
@@ -2,8 +2,10 @@ package org.bschlangaul.aufgaben.tech_info.assembler.mehr_adress;
 
 public class FibonacciRekursiv {
 
-  private static int fib(int n) {
-    if (n == 1 || n == 2)
+  public static int fib(int n) {
+    if (n <= 0)
+      return 0;
+    else if (n == 1 || n == 2)
       return 1;
     else
       return fib(n - 1) + fib(n - 2);
@@ -12,5 +14,17 @@ public class FibonacciRekursiv {
   public static void main(String[] args) {
     int n = 7;
     System.out.println(fib(n));
+
+    System.out.println(fib(0)); // 0
+    System.out.println(fib(1)); // 1
+    System.out.println(fib(2)); // 1
+    System.out.println(fib(3)); // 2
+    System.out.println(fib(4)); // 3
+    System.out.println(fib(5)); // 5
+    System.out.println(fib(6)); // 8
+    System.out.println(fib(7)); // 13
+    System.out.println(fib(8)); // 21
+    System.out.println(fib(9)); // 34
+    System.out.println(fib(10)); // 55
   }
 }
