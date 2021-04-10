b73de497ffd9013dc65475be2249f13cb3d62d01
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-04-10 15:07:11 +0200

    Baum

--- src/main/java/org/bschlangaul/examen/examen_66115/jahr_2020/herbst/Baum.java
+++ src/main/java/org/bschlangaul/examen/examen_66115/jahr_2020/herbst/Baum.java
@@ -8,7 +8,14 @@ public class Baum {
     if (i > (n - 2) / 2) {
       return true;
     }
+    if (A[i] <= A[2 * i + 1] && A[i] <= A[2 * i + 2] && magic(2 * i + 1, n) && magic(2 * i + 2, n)) {
+      return true;
+    }
+    return false;
+  }
 
+  public static boolean almostmagic(int i, int n) {
+    System.out.println(String.format("i: %s n: %s", i, n));
     if (A[i] <= A[2 * i + 1] && A[i] <= A[2 * i + 2] && magic(2 * i + 1, n) && magic(2 * i + 2, n)) {
       return true;
     }
@@ -17,6 +24,7 @@ public class Baum {
 
   public static void main(String[] args) {
     A = new int[] { 2, 4, 6, 14, 12, 10, 8, 22, 20, 12, 16 }; // true
+    System.out.println(A.length);
     System.out.println(magic(0, 10));
 
     // Aufgabe 2b)
@@ -32,5 +40,8 @@ public class Baum {
 
     A = new int[] { 2, 1, 3 };
     System.out.println(magic(0, 2)); // false
+
+    // A = new int[] { 1, 2, 3 };
+    // System.out.println(almostmagic(0, 4)); // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
   }
 }
