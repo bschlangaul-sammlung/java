ef4e71847d1bed085a1b93cc3abb8a84f11e0540
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-04-18 09:13:04 +0200

    Formatierung

--- src/main/java/org/bschlangaul/examen/examen_46115/jahr_2017/herbst/PrimzahlDP.java
+++ src/main/java/org/bschlangaul/examen/examen_46115/jahr_2017/herbst/PrimzahlDP.java
@@ -1,9 +1,7 @@
 package org.bschlangaul.examen.examen_46115.jahr_2017.herbst;
 
-
 /**
- * 1, 2, 3, 4, 5,  6,  7,  8,  9,  10
- * 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+ * 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
  */
 public class PrimzahlDP {
 
