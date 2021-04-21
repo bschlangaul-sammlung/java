bb7ca68f69c348d134adc3dd4f834884ac1b7515
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-04-21 18:26:25 +0200

    Variable umbenennen

--- src/main/java/org/bschlangaul/examen/examen_66115/jahr_2020/herbst/UngeradeGerade.java
+++ src/main/java/org/bschlangaul/examen/examen_66115/jahr_2020/herbst/UngeradeGerade.java
@@ -2,20 +2,20 @@ package org.bschlangaul.examen.examen_66115.jahr_2020.herbst;
 
 public class UngeradeGerade {
 
-  public static boolean suche(int[] feld, int suche) {
+  public static boolean suche(int[] feld, int schlüssel) {
     int links = 0, rechts = feld.length - 1;
-    boolean istGerade = suche % 2 == 0;
+    boolean istGerade = schlüssel % 2 == 0;
     int mitte = 0;
     while (links <= rechts) {
       mitte = links + (rechts - links) / 2;
-      if (feld[mitte] == suche) {
+      if (feld[mitte] == schlüssel) {
         return true;
       }
       // Verschiebe die linke Grenze nach rechts, wenn die gesuchte
       // Zahl gerade ist und die Zahl in der Mitte größer als die
       // gesuchte Zahl ist oder wenn die gesuchte Zahl ungerade ist
       // und die Zahl in der Mitte kleiner.
-      if ((istGerade && feld[mitte] > suche) || (!istGerade && feld[mitte] < suche)) {
+      if ((istGerade && feld[mitte] > schlüssel) || (!istGerade && feld[mitte] < schlüssel)) {
         // nach rechts verschieben
         links = mitte + 1;
       } else {
