b3ca6c266d3ddd5872a6435205539f44476a1803
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-04-21 18:25:45 +0200

    Gerade ungerade

--- src/main/java/org/bschlangaul/examen/examen_66115/jahr_2020/herbst/UngeradeGerade.java
+++ src/main/java/org/bschlangaul/examen/examen_66115/jahr_2020/herbst/UngeradeGerade.java
@@ -2,82 +2,31 @@ package org.bschlangaul.examen.examen_66115.jahr_2020.herbst;
 
 public class UngeradeGerade {
 
-  /**
-   * Eine klassische Binäre Suche, die in einem sortierten Feld eine
-   * Zahl findet. Kann so nicht auf die geforderte
-   * ungerade-aufsteigend-gerade-absteigend Folge angewendet werden.
-   *
-   * @param feld Ein aufsteigend sortiertes Feld.
-   * @param suche Eine Zahl nach der im Feld gesucht werden soll.
-   *
-   * @return Wahr, wenn die Zahl im Feld gefunden wurde.
-   */
-  public static boolean sucheBinär(int[] feld, int suche) {
-    int links = 0, rechts = feld.length - 1;
-    while (links <= rechts) {
-      int mitte = links + (rechts - links) / 2;
-      if (feld[mitte] == suche)
-        return true;
-      if (feld[mitte] < suche)
-        links = mitte + 1;
-      else
-        rechts = mitte - 1;
-    }
-    return false;
-  }
-
-  public static boolean sucheBinärUngeradeGerade(int[] feld, int suche) {
+  public static boolean suche(int[] feld, int suche) {
     int links = 0, rechts = feld.length - 1;
     boolean istGerade = suche % 2 == 0;
-    while (links <= rechts) {
-      int mitte = links + (rechts - links) / 2;
-      if (feld[mitte] == suche)
-        return true;
-      if (
-        // Verschiebe die linke Grenze nach rechts, wenn die gesuchte
-        // Zahl gerade ist und die Zahl in der Mitte größer als die
-        // gesuchte Zahl ist oder wenn die gesuchte Zahl ungerade ist
-        // und die Zahl in der Mitte kleiner.
-        (istGerade && feld[mitte] > suche)
-        ||
-        (!istGerade && feld[mitte] < suche)
-      )
-        // nach rechts verschieben
-        links = mitte + 1;
-      else
-        // nach links verschieben
-        rechts = mitte - 1;
-    }
-    return false;
-  }
-
-  /**
-   * Suche die Index-Nummer der letzten ungeraden Zahl.
-   *
-   * @param feld
-   * @return Die Index-Nummer der letzten ungeraden Zahl. Ist im Eingabe-Feld
-   *         keine ungerade Zahl vorhanden, dann wird -1 ausgegeben.
-   */
-  public static int sucheBinärLetzteUngerade(int[] feld) {
-    int links = 0, rechts = feld.length - 1;
     int mitte = 0;
     while (links <= rechts) {
       mitte = links + (rechts - links) / 2;
-      // ist Ungerade
-      if (feld[mitte] % 2 != 0)
+      if (feld[mitte] == suche) {
+        return true;
+      }
+      // Verschiebe die linke Grenze nach rechts, wenn die gesuchte
+      // Zahl gerade ist und die Zahl in der Mitte größer als die
+      // gesuchte Zahl ist oder wenn die gesuchte Zahl ungerade ist
+      // und die Zahl in der Mitte kleiner.
+      if ((istGerade && feld[mitte] > suche) || (!istGerade && feld[mitte] < suche)) {
+        // nach rechts verschieben
         links = mitte + 1;
-      else
+      } else {
+        // nach links verschieben
         rechts = mitte - 1;
+      }
     }
-    if (rechts < links)
-      return rechts;
-    return mitte;
+    return false;
   }
 
   public static void main(String[] args) {
-    // System.out.println(binarySearch(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
-    // 1));
-    System.out.println(sucheBinärLetzteUngerade(new int[] { 1, 3, 5, 7, 9, 10, 8, 6, 4, 2 }));
-
+    System.out.println(suche(new int[] { 1, 5, 11, 8, 4, 2 }, 4));
   }
 }
--- src/test/java/org/bschlangaul/examen/examen_66115/jahr_2020/herbst/UngeradeGeradeTest.java
+++ src/test/java/org/bschlangaul/examen/examen_66115/jahr_2020/herbst/UngeradeGeradeTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class UngeradeGeradeTest {
 
   private void assertSucheUnGerade(int[] feld, int suche, boolean ergebnis) {
-    assertEquals(ergebnis, UngeradeGerade.sucheBinärUngeradeGerade(feld, suche));
+    assertEquals(ergebnis, UngeradeGerade.suche(feld, suche));
   }
 
   @Test
@@ -16,20 +16,6 @@ public class UngeradeGeradeTest {
     assertSucheUnGerade(feld, 11, false);
     assertSucheUnGerade(feld, 0, false);
     assertSucheUnGerade(feld, 3, true);
-  }
-
-  private void assertLetzteUngerade(int[] feld, int index) {
-    assertEquals(index, UngeradeGerade.sucheBinärLetzteUngerade(feld));
-  }
-
-  @Test
-  public void letzteUngerade() {
-    assertLetzteUngerade(new int[] { 1, 3, 5, 7, 9, 10, 8, 6, 4, 2 }, 4);
-    assertLetzteUngerade(new int[] { 1, 3, 5, 7, 9 }, 4);
-    assertLetzteUngerade(new int[] { 10, 8, 6, 4, 2 }, -1);
-    assertLetzteUngerade(new int[] { 1, 10, 8, 6, 4, 2 }, 0);
-    assertLetzteUngerade(new int[] { 1, 49, 10, 8, 6, 4, 2 }, 1);
-    assertLetzteUngerade(new int[] { }, -1);
   }
 
 }
