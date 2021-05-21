9d43c4f11f03a101cd2b2eebe35d4a76a5b74998
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-05-21 20:38:50 +0200

    Palindrom

---
+++ src/main/java/org/bschlangaul/examen/examen_66116/jahr_2020/herbst/Palindrom.java
@@ -0,0 +1,34 @@
+package org.bschlangaul.examen.examen_66116.jahr_2020.herbst;
+
+public class Palindrom {
+
+  @SuppressWarnings({"unused"})
+  public static boolean istWortpalindrom(char[] wort) {
+    boolean resultat = false;
+    if (wort != null) {
+      int laenge = wort.length;
+      if (laenge >= 2) {
+        resultat = true;
+
+        for (int i = 0; i < laenge / 2; ++i) {
+          char c1 = wort[i];
+          char c2 = wort[laenge - 1 - i];
+          if (Character.toLowerCase(c1) != Character.toLowerCase(c2))
+            resultat = false;
+          break;
+        }
+      } else {
+        if (laenge == 1) {
+          resultat = true;
+        }
+      }
+    }
+    return resultat;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(istWortpalindrom(new char[] { 'a', 'b', 'a' }));
+    System.out.println(istWortpalindrom(new char[] { 'a', 'b', 'b', 'a' }));
+    System.out.println(istWortpalindrom(new char[] { 'a', 'b', 'b' }));
+  }
+}
