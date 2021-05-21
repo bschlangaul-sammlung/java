c747c87cf1cb00fb9be4ed51247906f7effb8bf7
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-05-21 21:06:07 +0200

    Zeilennummern

--- src/main/java/org/bschlangaul/examen/examen_66116/jahr_2020/herbst/Palindrom.java
+++ src/main/java/org/bschlangaul/examen/examen_66116/jahr_2020/herbst/Palindrom.java
@@ -3,28 +3,29 @@ package org.bschlangaul.examen.examen_66116.jahr_2020.herbst;
 public class Palindrom {
 
   @SuppressWarnings({"unused"})
-  public static boolean istWortpalindrom(char[] wort) {
-    boolean resultat = false;
-    if (wort != null) {
-      int laenge = wort.length;
-      if (laenge >= 2) {
-        resultat = true;
-
-        for (int i = 0; i < laenge / 2; ++i) {
-          char c1 = wort[i];
-          char c2 = wort[laenge - 1 - i];
-          if (Character.toLowerCase(c1) != Character.toLowerCase(c2))
-            resultat = false;
-          break;
-        }
-      } else {
-        if (laenge == 1) {
-          resultat = true;
-        }
-      }
-    }
-    return resultat;
-  }
+  public static boolean istWortpalindrom(char[] wort) { // 1
+    boolean resultat = false; // 2
+    if (wort != null) { // 3
+      int laenge = wort.length; // 4
+      if (laenge >= 2) { // 5
+        resultat = true; // 6
+        for (int i = 0; i < laenge / 2; ++i) { // 7
+          char c1 = wort[i]; // 8
+          char c2 = wort[laenge - 1 - i]; // 9
+          if (Character.toLowerCase(c1) != Character.toLowerCase(c2)) // 10
+          { // 11
+            resultat = false; // 12
+            break; // 13
+          } // 14
+        } // 15
+      } else { // 16
+        if (laenge == 1) { // 17
+          resultat = true; // 18
+        } // 19
+      } // 20
+    } // 21
+    return resultat; // 22
+  } // 23
 
   public static void main(String[] args) {
     System.out.println(istWortpalindrom(new char[] { 'a', 'b', 'a' }));
