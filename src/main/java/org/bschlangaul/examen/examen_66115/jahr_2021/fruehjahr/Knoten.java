7c9fbaaeb5ae687ca178b7abb113440970a91ae1
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-06-18 17:54:02 +0200

    Knoten

--- src/main/java/org/bschlangaul/examen/examen_66115/jahr_2021/fruehjahr/Knoten.java
+++ src/main/java/org/bschlangaul/examen/examen_66115/jahr_2021/fruehjahr/Knoten.java
@@ -1,32 +1,47 @@
 package org.bschlangaul.examen.examen_66115.jahr_2021.fruehjahr;
 
 public class Knoten {
-  Knoten links;
-  Knoten rechts;
-
-  String name;
-  /**
-   * Ebene
-   */
-  int y;
+  public Knoten links;
+  public Knoten rechts;
+  public String name;
 
   /**
    * Bewegung bezüglich des Vorknotens. Relative Lage.
    */
-  int x;
+  public int xVerschiebung;
 
-  void durchlaufenStart (Knoten knoten) {
-    durchlaufen(knoten, knoten.x, knoten.y);
+  public Knoten(String name, int xVerschiebung) {
+    this.name = name;
+    this.xVerschiebung = xVerschiebung;
   }
 
-  void durchlaufen (Knoten knoten, int x, int y) {
-    System.out.println(knoten.name + " " + knoten.x + " " + knoten.y);
+  public void durchlaufen() {
+    durchlaufe(this, 0 + xVerschiebung, 0);
+  }
+
+  private void durchlaufe(Knoten knoten, int x, int y) {
+    System.out.println("Beschriftung: " + knoten.name + " x: " + x + " y: " + y);
 
     if (links != null) {
-      links.durchlaufen(links, x + links.x, y + 1);
+      links.durchlaufe(links, x + links.xVerschiebung, y + 1);
     }
     if (rechts != null) {
-      rechts.durchlaufen(rechts, x + rechts.x, y + 1);
+      rechts.durchlaufe(rechts, x + rechts.xVerschiebung, y + 1);
     }
+  }
+
+  public static void main(String[] args) {
+    Knoten a = new Knoten("a", 1);
+    Knoten b = new Knoten("b", 1);
+    Knoten c = new Knoten("c", -2);
+    Knoten d = new Knoten("d", 2);
+    Knoten e = new Knoten("e", 0);
+
+    a.links = b;
+    a.rechts = c;
+    c.links = d;
+    c.rechts = e;
+
+    a.durchlaufen();
   }
 }
