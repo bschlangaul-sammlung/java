c5fa8a8143c18184a746db100cd798471df94d06
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-07-01 13:54:54 +0200

    sucheWertInFeld

--- src/main/java/org/bschlangaul/examen/examen_66116/jahr_2020/herbst/Verfikation.java
+++ src/main/java/org/bschlangaul/examen/examen_66116/jahr_2020/herbst/Verfikation.java
@@ -2,10 +2,19 @@ package org.bschlangaul.examen.examen_66116.jahr_2020.herbst;
 
 public class Verfikation {
 
-  public static void methode(int n, int m) {
-    int[] a = new int[n];
+  /**
+   * Suche in einem Feld nach einem Wert.
+   *
+   * @param a Ein Feld in dem nach einem Werte gesucht werden soll.
+   * @param m Der gesuchte Wert.
+   *
+   * @return Falls der Wert gefunden wurde, wird die Index-Nummer im
+   * Feld ausgegeben. Wenn der Wert nicht gefunden wurde, wird -1
+   * ausgegeben.
+   */
+  public static int sucheWertInFeld(int[] a, int m) {
+    int n = a.length;
 
-    a[4] = 10;
     int i = -1;
     // (1)
     int j = 0;
@@ -22,11 +31,15 @@ public class Verfikation {
         // (8)
       }
       // (9)
-      System.out.println(j);
     }
+
+    return i;
   }
 
   public static void main(String[] args) {
-    methode(10, 10);
+    int[] a = new int[10];
+    a[4] = 3;
+    System.out.println(sucheWertInFeld(a, 3)); // 4
+    System.out.println(sucheWertInFeld(a, 7)); // -1
   }
 }
