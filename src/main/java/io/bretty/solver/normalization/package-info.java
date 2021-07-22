0cd9eba6e55105c7d08702a2fcad50c5b85c60a9
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-07-22 19:46:45 +0200

    Füge DB normalization tool hinzu

---
+++ src/main/java/io/bretty/solver/normalization/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * A Java library to handle attributes and functional dependencies, and to solve
+ * database normalization related problems, including closure, minimal basis,
+ * projection, and decomposition to BCNF and 3NF, etc.
+ *
+ * see <a href=
+ * "https://github.com/nathanielove/Database-Normalization-Solver">Database-Normalization-Solver
+ * on Github</a>
+ */
+package io.bretty.solver.normalization;
