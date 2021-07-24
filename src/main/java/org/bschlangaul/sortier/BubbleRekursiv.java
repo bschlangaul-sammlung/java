759c93d24b39ce91d027cf060b31dfc061aff815
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-07-24 12:11:59 +0200

    Sortieren

--- src/main/java/org/bschlangaul/sortier/BubbleRekursiv.java
+++ src/main/java/org/bschlangaul/sortier/BubbleRekursiv.java
@@ -6,28 +6,27 @@ package org.bschlangaul.sortier;
 public class BubbleRekursiv extends Sortieralgorithmus {
 
   /**
-   * @param zahlen  Ein Feld mit Zahlen, das sortiert werden soll.
    * @param letztes Index-Nummer des Elements, bis zu dem sortiert werden soll.
    */
-  private void sortiereRekursiv(int[] zahlen, int letztes) {
+  private void sortiereRekursiv(int letztes) {
     // Bei einem leeren Feld kann die Variable letztes -1 annehmen.
     // Wenn letztes 0 ist, dann sind die Zahlen sortiert.
-    if (letztes <= 0)
+    if (letztes <= 0) {
       return;
-
+    }
     // Ein Durchgang der Blasensortierung. Nach diesem Durchgang befindet sich das
     // größte Element am Ende des Felds.
-    for (int i = 0; i < letztes; i++)
+    for (int i = 0; i < letztes; i++) {
       if (zahlen[i] > zahlen[i + 1]) {
         vertausche(i, i + 1);
       }
-
+    }
     // Sortiere jetzt die restlichen Elemente.
-    sortiereRekursiv(zahlen, letztes - 1);
+    sortiereRekursiv(letztes - 1);
   }
 
   public int[] sortiere() {
-    sortiereRekursiv(zahlen, zahlen.length - 1);
+    sortiereRekursiv(zahlen.length - 1);
     return zahlen;
   }
 
