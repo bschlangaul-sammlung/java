508563fe05e43e8142761b4ff0a1884ac5f2ebe0
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-07-31 15:29:01 +0200

    Teste die Halde

--- src/test/java/org/bschlangaul/baum/MinHaldeTest.java
+++ src/test/java/org/bschlangaul/baum/MinHaldeTest.java
@@ -10,7 +10,7 @@ public class MinHaldeTest {
     return schlüssel;
   }
 
-  private void vergleicheHaldenFeld(Comparable[] eingefügt, Comparable[] erwartet) {
+  private void vergleiche(Comparable[] eingefügt, Comparable[] erwartet) {
     MinHalde halde = new MinHalde<Integer>();
     for (int i = 0; i < eingefügt.length; i++) {
       halde.fügeEin(eingefügt[i]);
@@ -20,21 +20,26 @@ public class MinHaldeTest {
 
   @Test
   public void aufsteigendFünfZahlen() {
-    vergleicheHaldenFeld(feld(1, 2, 3, 4, 5), feld(1, 2, 3, 4, 5));
+    vergleiche(feld(1, 2, 3, 4, 5), feld(1, 2, 3, 4, 5));
   }
 
   @Test
   public void absteigendFünfZahlen() {
-    vergleicheHaldenFeld(feld(5, 4, 3, 2, 1), feld(1, 2, 4, 5, 3));
+    vergleiche(feld(5, 4, 3, 2, 1), feld(1, 2, 4, 5, 3));
   }
 
   @Test
   public void achtZahlen1() {
-    vergleicheHaldenFeld(feld(9, 5, 7, 1, 6, 4, 8, 2, 3), feld(1, 2, 4, 3, 6, 7, 8, 9, 5));
+    vergleiche(feld(9, 5, 7, 1, 6, 4, 8, 2, 3), feld(1, 2, 4, 3, 6, 7, 8, 9, 5));
   }
 
   @Test
   public void achtZahlen2() {
-    vergleicheHaldenFeld(feld(11, 1, 2, 13, 9, 10, 7, 5), feld(1, 5, 2, 9, 11, 10, 7, 13));
+    vergleiche(feld(11, 1, 2, 13, 9, 10, 7, 5), feld(1, 5, 2, 9, 11, 10, 7, 13));
+  }
+
+  @Test
+  public void examen66116jahr2015HerbstA7() {
+    vergleiche(feld(7, 1, 12, 8, 10, 3, 5), feld(1, 7, 3, 8, 10, 12, 5));
   }
 }
