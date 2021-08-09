2857af6bb672206936dc59d6e394705329f3403b
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-08-09 09:52:24 +0200

    Option Ordnung

--- src/main/java/org/bschlangaul/cli/KommandoBBaum.java
+++ src/main/java/org/bschlangaul/cli/KommandoBBaum.java
@@ -1,6 +1,7 @@
 package org.bschlangaul.cli;
 
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.util.List;
@@ -13,12 +14,16 @@ import org.bschlangaul.baum.report.TexBBaumAusgabe;
     "bb" }, mixinStandardHelpOptions = true, description = "Führe Aufgaben zum Thema BBaum aus.")
 class KommandoBBaum implements Callable<Integer> {
 
+  @Option(names = { "-o",
+      "--ordnung" }, defaultValue = "2", description = "Die Ordnung des B-Baums. Ist die Ordnung beispielsweise 2, dann muss jede Seite mindestens 2 Knoten und maximal 4 Knoten aufweisen.")
+  static int ordnung;
+
   @Parameters(arity = "1..*", description = "Einfügen: 1 2 3")
   List<String> werte;
 
   @Override
   public Integer call() {
-    BBaum bbaum = new BBaum(2);
+    BBaum bbaum = new BBaum(ordnung);
     for (int i = 0; i < werte.size(); i++) {
       String wert = werte.get(i);
       int zahl = Integer.parseInt(wert);
