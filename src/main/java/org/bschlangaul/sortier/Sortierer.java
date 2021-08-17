ceadce3ec15154955cb0e40a38449103d711947b
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-08-17 19:02:17 +0200

    Sortierer

--- src/main/java/org/bschlangaul/sortier/Sortierer.java
+++ src/main/java/org/bschlangaul/sortier/Sortierer.java
@@ -2,12 +2,16 @@ package org.bschlangaul.sortier;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * Hüllklasse mit der die verschiedenen Sortieralgorithmen über den Klassennamen
+ * aufgerufen werden können.
+ */
 public class Sortierer {
 
   public static int[] sortiere(String algorithmusKlassenName, int[] zahlen) {
     try {
-      Sortieralgorithmus algorithmus = (Sortieralgorithmus) Class.forName("org.bschlangaul.sortier." + algorithmusKlassenName)
-          .getDeclaredConstructor().newInstance();
+      Sortieralgorithmus algorithmus = (Sortieralgorithmus) Class
+          .forName("org.bschlangaul.sortier." + algorithmusKlassenName).getDeclaredConstructor().newInstance();
       algorithmus.setzeZahlen(zahlen);
       algorithmus.sortiere();
       return zahlen;
@@ -16,6 +20,23 @@ public class Sortierer {
       e.printStackTrace();
     }
     return zahlen;
+  }
+
+  /**
+   * Sortiere auch eine Zahlenreihe, die als String übergeben wird. Die Strings
+   * werden von Leerzeichen gesäubert und ein mögliches Komma entfernt.
+   *
+   * @param algorthmusKlassenName Der Klassenname eines Sortieralgorithmus.
+   * @param zahlen                Eine Zahlenreihe, die als String übergeben wird.
+   *
+   * @return Die sortiere Zahlenreihe als Integer-Feld.
+   */
+  public static int[] sortiere(String algorthmusKlassenName, String[] zahlen) {
+    int[] echteZahlen = new int[zahlen.length];
+    for (int i = 0; i < zahlen.length; i++) {
+      echteZahlen[i] = Integer.parseInt(zahlen[i].replaceAll(",", "").trim());
+    }
+    return sortiere(algorthmusKlassenName, echteZahlen);
   }
 
   public static void main(String[] args) {
