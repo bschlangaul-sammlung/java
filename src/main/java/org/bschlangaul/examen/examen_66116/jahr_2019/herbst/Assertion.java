014f46f5b3ef14ecc5bd98c33e8e521722a22769
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-09-01 19:00:37 +0200

    Assertions

--- src/main/java/org/bschlangaul/examen/examen_66116/jahr_2019/herbst/Assertion.java
+++ src/main/java/org/bschlangaul/examen/examen_66116/jahr_2019/herbst/Assertion.java
@@ -10,18 +10,53 @@ public class Assertion {
     return C;
   }
 
-  public static double[][] magic2(double[][] A, double[][] B, int m) {
+  private static void assert2DArray(double[][] a, int m) throws Exception {
+    if (a.length < m) {
+      throw new Exception("Das 2D-Feld muss mindestens m Zeilen/Felder haben.");
+    }
+
+    for (int i = 0; i < a.length; i++) {
+      double[] row = a[i];
+      if (row.length < m) {
+        throw new Exception("Jede Zeile im 2D-Feld muss mindestens m Einträge/Spalten haben.");
+      }
+    }
+  }
+
+  public static double[][] magicWithAssertions(double[][] A, double[][] B, int m) throws Exception {
+    if (m < 0) {
+      throw new Exception("m darf nicht negativ sein.");
+    }
+    assert2DArray(A, m);
+    assert2DArray(B, m);
     double[][] C = new double[m][m];
     for (int i = 0; i < m; i++)
       for (int j = 0; j < m; j++)
         for (int k = 0; k < m; k++) {
           C[i][j] += A[i][k] * B[k][j];
+        }
+    return C;
+  }
 
-          if (i == 2  && j == 2) {
+  /**
+   * Duplikat der magic-Methode mit einer println-Anweisung, um die Funktionsweise besser untersuchen zu können.
+   *
+   * @param A Ein zwei-dimensionales Feld des Datentyps double. Sollte mindestens m x m Einträge haben.
+   * @param B Ein zwei-dimensionales Feld des Datentyps double. Sollte mindestens m x m Einträge haben.
+   * @param m Ein nicht negative Ganzzahl.
+   *
+   * @return Ein zwei-dimensionales Feld mit m x m Einträgen.
+   */
+  public static double[][] magicPrint(double[][] A, double[][] B, int m) {
+    double[][] C = new double[m][m];
+    for (int i = 0; i < m; i++)
+      for (int j = 0; j < m; j++)
+        for (int k = 0; k < m; k++) {
+          C[i][j] += A[i][k] * B[k][j];
+          if (i == 2 && j == 2) {
             System.out.println(String.format("A[%d][%d] * B[%d][%d] = %s * %s", i, k, k, j, A[i][k], B[k][j]));
           }
         }
-
     return C;
   }
 
@@ -36,7 +71,11 @@ public class Assertion {
   }
 
   public static void printMagic(double[][] A, double[][] B, int m) {
-    print(magic2(A, B, m));
+    try {
+      print(magicPrint(A, B, m));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args) {
@@ -45,7 +84,7 @@ public class Assertion {
       { 4, 5, 6 },
       { 7, 8, 9 },
     }, new double[][] {
-      { 11, 12, 13, 1000 },
+      { 11, 12, 13 },
       { 16, 15, 14 },
       { 17, 18, 19 },
     }, 3);
